4677a0feb88d92fe167a82be090dc847a65fa895
dimkael <53169743+dimkael@users.noreply.github.com>
2020-02-09 13:31:43 +0300

    Edited php file

--- Universal edit database PHP script/dbOutputAndEdit.php
+++ Universal edit database PHP script/dbOutputAndEdit.php
@@ -2,8 +2,16 @@
 <h3>Current table</h3>
 <table>
     <?php
-        require_once 'dbconnect.php';
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $database = 'test';
         $table = 'users';
+
+
+       $link = mysqli_connect($host, $user, $password, $database) or die(mysqli_error($link));
+        mysqli_query($link, "SET NAMES 'utf8'");
+
 
         function createQuery($keys, $values, $table) {
             $query = "INSERT INTO `$table` SET";
